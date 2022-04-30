14cf2fcdd18525ab4d9774b4914dc3aa05b6d5f3
apple <applezyh@outlook.com>
2022-04-30 22:17:37 +0800

    update README

--- example/MinNet.cpp
+++ example/MinNet.cpp
@@ -34,15 +34,23 @@ private:
     minnet::Linear fc1;
 };
 
-#define NUM 600
-#define BATCH_SIZE 32
+#define NUM 60000
 
 #define SHOW_RESULT
 
-int main() {
+int main(int argc, char**argv) {
+    if(argc != 3){
+        std::cout<<"must input mnist dataset data path and label path"<<std::endl;
+        return 0;
+    }
     srand(clock());
-    auto src_data = load_mnist("/media/sf_train-images-idx3-ubyte/train-images.idx3-ubyte", "/media/sf_train-labels-idx1-ubyte/train-labels.idx1-ubyte");
-   // auto src_data = load_cifa10("C:\\Users\\apple\\Downloads\\cifar-10-binary\\cifar-10-batches-bin");
+    auto src_data = load_mnist(argv[1], argv[2]);
+
+    if (src_data.size() != 60000) {
+        std::cout<<"read dataset erro!"<<std::endl;
+        return 0;
+    }
+
     std::vector<std::vector<float>> data(NUM);
     std::vector<std::vector<float>> label(NUM);
 
@@ -51,8 +59,6 @@ int main() {
         label[i] = std::vector<float>(10, 0.f);
         label[i][src_data[i].first] = 1.f;
     }
-
-    //std::string classes[] = { "airplane" , "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck" };
     
     Net net;
     float lr = 0.0004f;
@@ -100,14 +106,3 @@ int main() {
 #endif // SHOW_RESULT
     return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
